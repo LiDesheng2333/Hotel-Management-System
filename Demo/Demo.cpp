576f190849a3fc92a845a24c67e61bb5f5916478
QzwQ456 <qzwq456@gmail.com>
2021-05-30 17:31:34 +0800

    添加项目文件。

---
+++ Demo/Demo.cpp
@@ -0,0 +1,9 @@
+﻿#include<stdio.h>
+int main()
+{
+	int a;
+	printf("\n\n\t\t\t\t\t  ************酒店管理系统************\n");
+	printf("\n\t\t\t\t\t    录入客房信息            [A]\n\t\t\t\t\t    查询客房信息            [B]\n\t\t\t\t\t    入住业务                [C]\n\t\t\t\t\t    退房业务                [D]\n\t\t\t\t\t    统计查询                [E]\n\t\t\t\t\t    退出系统                [F]\n");
+	printf("\n\t\t\t输入字母选项:");
+	return 0;
+}
